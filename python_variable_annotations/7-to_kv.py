2b8880e4ff377967ef09512991870bca5cedc42b
TheKasman <kasapidisandrew@gmail.com>
2026-03-26 16:09:58 +1100

    linting

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """Question 7"""
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """string and int/float to tuple"""
     return (k, float(v ** 2))
