50154efa947eabec33a54504b41ea4f0611f375b
TheKasman <kasapidisandrew@gmail.com>
2026-03-26 14:49:41 +1100

    yay i guess?

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,6 +2,7 @@
 """2 to the floor, i was sure...? doesn't sound right"""
 import math
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """Returns the floor of a float"""
+    print(repr(math.floor(n)))
     return math.floor(n)
