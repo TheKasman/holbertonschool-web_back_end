d1109b225e8c941f1e04843ef187c6905ff6eb1b
TheKasman <kasapidisandrew@gmail.com>
2026-03-26 14:41:54 +1100

    so we CAN use floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """2 to the floor, i was sure...? doesn't sound right"""
-
+import math
 
 def floor(n: float) -> float:
     """Returns the floor of a float"""
-    return n[0:]
+    return floor(n)
