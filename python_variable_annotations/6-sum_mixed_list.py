f96e3d0fc3dfb2f0dbc9c76bc1f24e2ae6e20d81
TheKasman <kasapidisandrew@gmail.com>
2026-03-26 15:31:23 +1100

    Union

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Question 6"""
+from typing import List, Union
+
+
+def sum_mixed_list(input_list: List[Union[int, float]]) -> float:
+    """Ints and floats sum"""
+    return sum(input_list)
