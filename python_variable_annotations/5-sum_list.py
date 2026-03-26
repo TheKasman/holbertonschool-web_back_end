540f028442f09e427037231762bed2f429c29a59
TheKasman <kasapidisandrew@gmail.com>
2026-03-26 15:08:41 +1100

    added import for typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Question 5"""
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Sum up a list!"""
     return sum(input_list)
