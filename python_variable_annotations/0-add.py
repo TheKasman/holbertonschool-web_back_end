c44b68e0b431274377e57cd169d935bb5bab4f15
TheKasman <kasapidisandrew@gmail.com>
2026-03-26 14:29:00 +1100

    shebang fixup

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env python3
 """Time for annotations!"""
 from typing import Annotated
+
 
 def add(number_1: float,  number_2: float) -> float:
     return number_1 + number_2
