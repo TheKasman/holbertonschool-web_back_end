5f7e735cd59393f25b75c11d46209ab13b8accbb
TheKasman <kasapidisandrew@gmail.com>
2026-03-26 15:49:58 +1100

    refactor to old style

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Question 7"""
+from typing import Tuple, Union
 
-
-def to_kv(k: str, v: int | float) -> tuple[str, int | float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """string and int/float to tuple"""
     return (k, v)
