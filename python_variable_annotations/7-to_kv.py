9576204df3d139e338cb2fb3bf52da7e4d434355
TheKasman <kasapidisandrew@gmail.com>
2026-03-26 15:54:31 +1100

    union unifyin

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """Question 7"""
 from typing import Tuple, Union
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
     """string and int/float to tuple"""
-    return (k, v)
+    return (k, float(v))
+ 
