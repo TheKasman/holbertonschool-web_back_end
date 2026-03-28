52f827f3fb518900171d04dd2deb053c98faf635
TheKasman <kasapidisandrew@gmail.com>
2026-03-28 11:20:22 +1100

    third last for today

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Question 8"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Multiplies by a value"""
+    def multiply(value: float) -> float:
+        return multiplier * value
+    return multiply
