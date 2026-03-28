184c86e456c76aaa74f4b63bda631d2d25ab9f06
TheKasman <kasapidisandrew@gmail.com>
2026-03-28 11:30:40 +1100

    linting

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """Final question for the module"""
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Returns the length of an element"""
     return [(i, len(i)) for i in lst]
