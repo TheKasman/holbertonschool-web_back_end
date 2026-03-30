c8969eef4ee60de19a4d48ba72439c0ae0137e6f
TheKasman <kasapidisandrew@gmail.com>
2026-03-30 15:32:37 +1100

    changed output to a list of floats, dereferenced tasks

---
+++ python_async_function/1-concorrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Question 1 - Wait_n"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """Spawns wait_random n times, returns float values"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    results = await asyncio.gather(*tasks)
+    return results
