e4d7d34b29e9c355f104af4ff09a958771ad2a5b
TheKasman <kasapidisandrew@gmail.com>
2026-03-31 15:31:36 +1100

    added type import

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Question 1 - Wait_n"""
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n times, returns float values"""
     tasks = [wait_random(max_delay) for i in range(n)]
     results = []
