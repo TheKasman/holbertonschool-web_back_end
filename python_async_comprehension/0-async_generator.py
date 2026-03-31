63514c9e60c0f2ab6563a5b790aff6d0ced60dfd
TheKasman <kasapidisandrew@gmail.com>
2026-03-31 20:42:15 +1100

    explicitly removing 0 and 10 from the equation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,4 +8,8 @@ async def async_generator():
     """loop 10 times, wait 1 second, yield random number from 0-10"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        while True:
+            x = random.random() * 10
+            if x != 0:
+                yield x
+                break
