e03dc3dcfa3a6b4dee318f96880a5afc8bfeb200
TheKasman <kasapidisandrew@gmail.com>
2026-03-31 15:49:09 +1100

    attempt 1

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Question 2  - working with question 0 and 1"""
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """do more processes and return a float"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time / n
