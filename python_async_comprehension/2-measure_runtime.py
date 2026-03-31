be84db5af3eecd514e2f93fa9a268cefd613af57
TheKasman <kasapidisandrew@gmail.com>
2026-04-01 10:25:54 +1100

    linting

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Run async_comprehension 4 times and return the runtime"""
     start_runtime = time.time()
-    await asyncio.gather(*(async_comprehension() for _ in range (4)))
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_runtime = time.time()
     return end_runtime - start_runtime
