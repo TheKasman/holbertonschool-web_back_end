bd8d74c650b7e5546f75e3b2aac29245326db7fe
TheKasman <kasapidisandrew@gmail.com>
2026-03-31 21:18:05 +1100

    last one!

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Now we use question 1 to solve 2"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Run async_comprehension 4 times and return the runtime"""
+    start_runtime = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_runtime = time.time()
+    return end_runtime - start_runtime
