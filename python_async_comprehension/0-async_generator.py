fe39ec76e196278da8aaf4a4ea8121a4e0b3eb80
TheKasman <kasapidisandrew@gmail.com>
2026-03-31 20:34:00 +1100

    changed random.uniform -> random.random * 10

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,4 +8,4 @@ async def async_generator():
     """loop 10 times, wait 1 second, yield random number from 0-10"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
