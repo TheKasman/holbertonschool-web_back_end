f863ec039457706630285cc0144e234543fe43ca
TheKasman <kasapidisandrew@gmail.com>
2026-03-31 20:56:14 +1100

    added typecast for the output

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,14 +2,10 @@
 """Async generator"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
-
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """loop 10 times, wait 1 second, yield random number from 0-10"""
     for _ in range(10):
         await asyncio.sleep(1)
-        while True:
-            x = random.random() * 10
-            if x != 0:
-                yield x
-                break
+        yield random.uniform(0, 10)
