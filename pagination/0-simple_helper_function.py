b4b5d80f13f7fc7478c10a23539f528d522811d2
TheKasman <kasapidisandrew@gmail.com>
2026-04-15 18:52:36 +1000

    Question 0 and readme

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-"""New work for sprint 3!""""
+"""New work for sprint 3!"""
 
-    def index_range(page, page_size):
-    """ returns a tuple of size two  """
+
+def index_range(page: int, page_size: int) -> tuple:
+    """ returns a tuple of size two"""
+    start = (page - 1) * page_size
+    end = page + page_size
+    return (start, end)
+
