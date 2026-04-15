995021d13c9b954c5614d8fb2fee77d76187fe4f
TheKasman <kasapidisandrew@gmail.com>
2026-04-15 19:00:32 +1000

    Fix #2

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """New work for sprint 3!"""
+from typing import Tuple
 
-
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ returns a tuple of size two containing the start and end indexes"""
     start = (page - 1) * page_size
     end = start + page_size
