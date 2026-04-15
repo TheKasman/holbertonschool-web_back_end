6c6eaa0d963824594c8cae9053c72b16a9d499d3
TheKasman <kasapidisandrew@gmail.com>
2026-04-15 19:02:55 +1000

    Pep8 fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """New work for sprint 3!"""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ returns a tuple of size two containing the start and end indexes"""
     start = (page - 1) * page_size
