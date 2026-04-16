795c17844697537c4118cff1ac4682b0a45a9b3e
TheKasman <kasapidisandrew@gmail.com>
2026-04-16 14:51:14 +1000

    added asserts to get_hyper

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -34,6 +34,9 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         """Return pagination metadata and data"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
         return {
