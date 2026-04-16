9415a1ef437c7dac8ef566a4d58c4457afd061b2
TheKasman <kasapidisandrew@gmail.com>
2026-04-16 15:05:51 +1000

    assistance taken

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -37,14 +37,21 @@ class Server:
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
-        data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset()) / page_size)
+        returned_data = self.get_page(page, page_size)
+        data = self.dataset()
+        data_length = len(data)
+        total_pages = math.ceil(data_length / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        
+        prev_page = page + 1 if page < total_pages else None
         return {
-            "page_size": len(data),
+            "page_size": len(returned_data),
             "page": page,
             "data": data,
-            "next_page": page + 1 if page < total_pages else None,
-            "prev_page": page - 1 if page > 1 else None
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
         }
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
