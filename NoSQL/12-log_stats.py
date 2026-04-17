182d6e48fa1d40a2746428de46c87755a1634e14
TheKasman <kasapidisandrew@gmail.com>
2026-04-17 18:03:02 +1000

    fixing some typos in the script

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for m in methods:
-        count = collection.count_documents({"method: m"})
+        count = collection.count_documents({"method": m})
         print(f"\tmethod {m}: {count}")
 
     status_count = collection.count_documents(
-        {"method:" "GET", "path", "/status"}
+        {"method": "GET", "path": "/status"}
     )
     print(f"{status_count} status check")
