8d6417d8dcae45900fa1f8f86b2b48d7c69e5771
TheKasman <kasapidisandrew@gmail.com>
2026-04-24 19:13:52 +1000

    changed to arrow function

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
-  }
-
-  return array;
+  return array.map((value) => appendString + value);
 }
