acd5a0093a526b9713ab2315304c6deba45a6786
TheKasman <kasapidisandrew@gmail.com>
2026-04-26 15:37:52 +1000

    iteration 1

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name = '', code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return this._code;
+  } 
+
+  get [Symbol.toStringTag]() {
+    return `Airport [${this._code}]`;
+  }
+}
