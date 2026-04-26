cd65ef5e6b6dab4efd145c589a37ce5fe4871531
TheKasman <kasapidisandrew@gmail.com>
2026-04-26 13:52:58 +1000

    question 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,42 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string' ) {
+      throw TypeError('code must be a string');
+    }
+
+    if (typeof name !== 'string') {
+      throw TypeError('name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) { 
+    if (typeof code !== 'string' ) {
+      throw TypeError('code must be a string');
+    }
+
+    this._code = newCode; 
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof name !== 'string') {
+      throw TypeError('name must be a string');
+    }
+
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})` 
+  }
+}
