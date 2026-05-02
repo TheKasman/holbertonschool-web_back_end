2c363796b0f062fcea2feff6d5b66b12e237cbd7
TheKasman <kasapidisandrew@gmail.com>
2026-05-02 21:46:22 +1000

    forgot a check for bad values or empty string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
   const list = [];
 
+  if (startString === "" || typeof startString !== "string" ){
+    return "";
+  }
   for (const value of set) {
     if (value.startsWith(startString)) {
       list.push(value.slice(startString.length)); 
