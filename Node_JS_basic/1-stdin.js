c955065f250b3a3a407701268974e78d15357b13
TheKasman <kasapidisandrew@gmail.com>
2026-05-06 22:05:55 +1000

    question 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,10 +2,9 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
-  console.log('This important software is now closing');
-  process.exit(1);
+  process.stdin.pause();
 });
 
-process.stdin.on('exit', () => {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
