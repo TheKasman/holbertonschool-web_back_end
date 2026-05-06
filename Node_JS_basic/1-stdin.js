2c0a339f4aa83384301b6365190e14fd4973e1fe
TheKasman <kasapidisandrew@gmail.com>
2026-05-06 22:23:00 +1000

    Question 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,11 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
-  process.stdin.pause();
+
+  // If interactive, manually close
+  if (process.stdin.isTTY) {
+    process.stdin.pause();
+  }
 });
 
 process.stdin.on('end', () => {
