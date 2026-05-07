ed5a1bc10cc6a8e260344fd1246d86cd50df061e
TheKasman <kasapidisandrew@gmail.com>
2026-05-07 12:09:01 +1000

    okay one more time

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,11 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
-
-  // If interactive, manually close
-  if (process.stdin.isTTY) {
-    process.stdin.pause();
-  }
+  process.stdin.pause();
 });
 
 process.stdin.on('end', () => {
