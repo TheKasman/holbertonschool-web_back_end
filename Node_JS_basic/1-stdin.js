4f29cd634e6609d71597efbb0015b36303c118b6
TheKasman <kasapidisandrew@gmail.com>
2026-05-12 10:59:27 +1000

    added :

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
   process.stdin.pause();
 });
 
