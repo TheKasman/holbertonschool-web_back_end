2fab4e264094015f6c57c3a46150950e1c6d441f
TheKasman <kasapidisandrew@gmail.com>
2026-05-12 13:45:18 +1000

    brief fitting

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,9 +3,9 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  res.end('Hello Holberton School\n');
+  res.end('Hello Holberton School!\n');
 });
 
-app.listen(12345);
+app.listen(1245);
 
 module.exports = app;
