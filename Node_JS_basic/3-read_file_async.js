1c719763762f6c3041a476e59a4c181b8c630dcf
TheKasman <kasapidisandrew@gmail.com>
2026-05-12 13:31:52 +1000

    question 3 done

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+function countStudents(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data
+        .trim()
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+
+      const students = lines.slice(1);
+
+      console.log(`Number of students: ${students.length}`);
+
+      const fields = {};
+
+      for (const row of students) {
+        const parts = row.split(',');
+        const firstname = parts[0];
+        const field = parts[3];
+
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      }
+
+      for (const field of Object.keys(fields)) {
+        console.log(
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+        );
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
+
