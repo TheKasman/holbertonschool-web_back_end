8979a942b9967ce94b0969ec46a4b82c7138457d
TheKasman <kasapidisandrew@gmail.com>
2026-05-15 14:14:46 +1000

    question 7 done

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+const dbFile = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.type('text');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(dbFile)
+    .then((output) => {
+      res.type('text');
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch(() => {
+      res.type('text');
+      res.send('This is the list of our students\nCannot load the database');
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server is now listening on ${port}`);
+});
+
+module.exports = app;
